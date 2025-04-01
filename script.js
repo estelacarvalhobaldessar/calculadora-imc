8f9dc677f5b43313d7a31a165aa09134739434ff
Estela de C. Baldessar <ecbaldessar@gmail.com>
2025-04-01 08:21:45 -0300

    teste2

--- script.js
+++ script.js
@@ -8,9 +8,18 @@ btnCalcular.addEventListener("click", function () {
    if ( calculo <= 18.5) {
     conceito = 'Baixo peso';
    } else if( calculo > 18.5 && calculo < 24.9){
-    conceito = 'Peso Normal'
-   } else if(){
-
-   } 
+    conceito = 'Peso Normal';
+   } else if( calculo >= 25 && calculo < 29.9){
+    conceito = 'Excesso de Peso';
+   } else if (calculo  >= 30) {
+    conceito = 'Obesidade';  
+   } else if (calculo >= 30 && calculo < 34.9) {
+    conceito = 'Obesidade Classe 1';
+   } else if (calculo >= 35 && calculo < 39.9) {
+    conceito = 'Obesidade Classe 2'; 
+   } else{
+      conceito = 'Obesidade Mórbida';
+   }
+   document.getElementById("resultado").innerHTML = calculo+' '+conceito;
 });
 
